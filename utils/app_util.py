d28e40f7a734c4acd67c2d8eb8f64486885069f7
darrylmorton <darryl.j.morton@gmail.com>
2025-12-22 23:06:48 +0000

    chore: pr request changes

--- utils/app_util.py
+++ utils/app_util.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import toml
@@ -27,7 +28,15 @@ class AppUtil:
             package_json_file = Path(__file__).parent.parent.parent / "package.json"
 
             if package_json_file.exists() and package_json_file.is_file():
-                app_version = toml.load(package_json_file)["version"]
+                # app_version = json.(package_json_file)["version"]
+                try:
+                    with open(package_json_file, 'r') as file:
+                        data = json.load(file)
+                    # print("File data =", data)
+                    app_version = data["version"]
+
+                except FileNotFoundError:
+                    print("Error: The file 'data.json' was not found.")
 
         if package_manager == "poetry":
             pyproject_toml_file = Path(__file__).parent.parent.parent / "pyproject.toml"
